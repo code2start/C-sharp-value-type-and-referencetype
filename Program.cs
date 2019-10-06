58108a55adda7167d1acf6549558fc0c7ca7ece8
Mohamed Fathy <codetwostart@gmail.com>
2019-10-06 21:01:58 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace valuetypeandreferencetype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int a = 12;
+            Console.WriteLine(a);
+            string text = "Hello";
+            */
+            string name = default;
+            Console.WriteLine(name);
+        }
+    }
+}
